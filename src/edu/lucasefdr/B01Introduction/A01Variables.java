549e14fda3515d0b63dfb3815dc4d70da0bb7ae3
Lucas <lucas.efdrosa@gmail.com>
2022-11-18 16:27:47 -0300

    casting double to int

--- src/edu/lucasefdr/B01Introduction/A01Variables.java
+++ src/edu/lucasefdr/B01Introduction/A01Variables.java
@@ -10,11 +10,16 @@ public class A01Variables {
         System.out.println(intAge);
 
         // declarando variáveis numéricas através de var e double
-        var varSalary = 2000.0;
+        var varSalary = 2000.95;
         double doubleSalary;
-        doubleSalary = 4900.0;
+        doubleSalary = 4900.10;
 
         System.out.println(varSalary);
         System.out.println(doubleSalary);
+
+        // casting de double para int
+        int castDoubleToInt = (int) varSalary;
+
+        System.out.println(castDoubleToInt);
     }
 }
