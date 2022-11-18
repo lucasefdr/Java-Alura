f52674a3c2b59dc3fca15cf38ad86092b97603d5
Lucas <lucas.efdrosa@gmail.com>
2022-11-18 16:09:23 -0300

    var, int

---
+++ src/edu/lucasefdr/B01Introduction/A01Variables.java
@@ -0,0 +1,12 @@
+package edu.lucasefdr.B01Introduction;
+
+public class A01Variables {
+    public static void main(String... args) {
+        // declarando variáveis numéricas através de var e int
+        var varAge = 22;
+        int intAge = 24;
+
+        System.out.println(varAge);
+        System.out.println(intAge);
+    }
+}
