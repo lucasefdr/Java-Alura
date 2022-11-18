f32ea095f8efb19b29cc8e70e5ebe808df4cc7b6
Lucas <lucas.efdrosa@gmail.com>
2022-11-18 16:14:29 -0300

    var, double

--- src/edu/lucasefdr/B01Introduction/A01Variables.java
+++ src/edu/lucasefdr/B01Introduction/A01Variables.java
@@ -8,5 +8,13 @@ public class A01Variables {
 
         System.out.println(varAge);
         System.out.println(intAge);
+
+        // declarando variáveis numéricas através de var e double
+        var varSalary = 2000.0;
+        double doubleSalary;
+        doubleSalary = 4900.0;
+
+        System.out.println(varSalary);
+        System.out.println(doubleSalary);
     }
 }
