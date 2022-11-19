4d36a290a621ee2114eb9d76f3d0016102e99d7b
Lucas <lucas.efdrosa@gmail.com>
2022-11-19 10:59:10 -0300

    Control structures: while

---
+++ src/edu/lucasefdr/B01Introduction/A04ControlStructures.java
@@ -0,0 +1,12 @@
+package edu.lucasefdr.B01Introduction;
+
+public class A04ControlStructures {
+    public static void main(String[] args) {
+        int count = 1;
+
+        while (count <= 10) {
+            System.out.println(count);
+            count++;
+        }
+    }
+}
