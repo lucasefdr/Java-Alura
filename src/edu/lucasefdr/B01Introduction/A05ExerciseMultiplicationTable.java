76263e96f1e8dd7499fec14602c62ff8e43d3454
Lucas <lucas.efdrosa@gmail.com>
2022-11-19 11:17:41 -0300

    Exercise: multiplication table

---
+++ src/edu/lucasefdr/B01Introduction/A05ExerciseMultiplicationTable.java
@@ -0,0 +1,14 @@
+package edu.lucasefdr.B01Introduction;
+
+public class A05ExerciseMultiplicationTable {
+    public static void main(String[] args) {
+        System.out.println("Tabuadas do 1 ao 10");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Tabuada do " + i);
+            for (int j = 1; j <= 10; j++) {
+                System.out.printf("%d x %d = %d%n", i, j, i * j);
+            }
+        }
+    }
+}
