2dc2b81d670e432986ca0cd407d631d14ab970a3
Lucas <lucas.efdrosa@gmail.com>
2022-11-20 08:23:01 -0300

    Object reference

--- src/edu/lucasefdr/B02OOP/test/ContaTeste.java
+++ src/edu/lucasefdr/B02OOP/test/ContaTeste.java
@@ -8,5 +8,17 @@ public class ContaTeste {
         Conta primeiraConta = new Conta();
         primeiraConta.saldo = 200;
         System.out.println(primeiraConta.saldo);
+
+        Conta segundaConta = new Conta();
+        segundaConta.saldo = 250;
+        System.out.println(segundaConta.saldo);
+
+        // os objetos são referências de memória e não valor
+        Conta terceiraConta = primeiraConta;
+        terceiraConta.saldo = 500;
+        System.out.println(primeiraConta);
+        System.out.println(terceiraConta);
+
+        if (primeiraConta == terceiraConta) System.out.println("é a mesma conta");
     }
 }
