1867d22ff051d439296d2cec4a9eed0b8e8c31ab
Lucas <lucas.efdrosa@gmail.com>
2022-11-21 21:24:29 -0300

    Polymorphism

---
+++ src/edu/lucasefdr/B03OOP2/model/ControleBonificacao.java
@@ -0,0 +1,15 @@
+package edu.lucasefdr.B03OOP2.model;
+
+public class ControleBonificacao {
+
+    private double soma;
+
+    public void registra(Funcionario funcionario) {
+        double bonificacao = funcionario.getBonificacao();
+        this.soma = this.soma + bonificacao;
+    }
+
+    public double getSoma() {
+        return soma;
+    }
+}
--- src/edu/lucasefdr/B03OOP2/test/TestGerente.java
+++ src/edu/lucasefdr/B03OOP2/test/TestGerente.java
@@ -1,10 +1,20 @@
 package edu.lucasefdr.B03OOP2.test;
 
+import edu.lucasefdr.B03OOP2.model.ControleBonificacao;
+import edu.lucasefdr.B03OOP2.model.Funcionario;
 import edu.lucasefdr.B03OOP2.model.Gerente;
 
 public class TestGerente {
     public static void main(String[] args) {
-        Gerente eduardo = new Gerente("Eduardo Ferreira", "444-111-999-99", 8000);
-        eduardo.imprimeInformacoes();
+        Gerente gerente = new Gerente("Eduardo Ferreira", "444-111-999-99", 8000);
+        gerente.imprimeInformacoes();
+
+        // POLIMORFISMO: é possível instanciar um objeto através de uma referência mais genérica
+        Funcionario funcionario = new Gerente();
+
+        // O método recebe um funcionário como parâmetro, porém, Gerente É um Funcionário
+        ControleBonificacao controleBonificacao = new ControleBonificacao();
+        controleBonificacao.registra(gerente);
+        System.out.println(controleBonificacao.getSoma());
     }
 }
