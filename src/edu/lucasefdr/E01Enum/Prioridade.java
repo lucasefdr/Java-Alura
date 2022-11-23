e14729296783942aa420249b454742d99f560c89
Lucas <lucas.efdrosa@gmail.com>
2022-11-22 22:11:34 -0300

    Enum

---
+++ src/edu/lucasefdr/E01Enum/Prioridade.java
@@ -0,0 +1,20 @@
+package edu.lucasefdr.E01Enum;
+
+/**
+ * <strong>Enum</strong> serve para criarmos constantes.<br>
+ * Não é possível instanciar uma enumeração.
+ */
+public enum Prioridade {
+    // 0, 1, 2...
+    MIN(1), NORMAL(5), MAX(10);
+
+    private final int valor;
+
+    Prioridade(int valor) {
+        this.valor = valor;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+}
