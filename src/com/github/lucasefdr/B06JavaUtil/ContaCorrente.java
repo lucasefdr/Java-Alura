d7a3def62775d76039844929d9a045e626079bd9
Lucas <lucas.efdrosa@gmail.com>
2022-11-26 12:23:32 -0300

    Array with reference types

---
+++ src/com/github/lucasefdr/B06JavaUtil/ContaCorrente.java
@@ -0,0 +1,35 @@
+package com.github.lucasefdr.B06JavaUtil;
+
+import com.github.lucasefdr.B03OOP2.exception.SacaException;
+import com.github.lucasefdr.B03OOP2.model.banco.Tributavel;
+
+public class ContaCorrente extends Conta implements Tributavel {
+    public ContaCorrente() {
+    }
+
+    public ContaCorrente(int numero, int agencia) {
+        super(numero, agencia);
+    }
+
+    @Override
+    public void saca(double valor) throws SacaException {
+        double descontoSaque = valor * 0.02;
+        valor += descontoSaque;
+        super.saca(valor);
+    }
+
+    @Override
+    public void deposita(double valor) {
+        super.saldo += valor;
+    }
+
+    @Override
+    public double getValorImposto() {
+        return super.saldo * 0.1;
+    }
+
+    @Override
+    public String toString() {
+        return "Conta Corrente: " + super.toString();
+    }
+}
