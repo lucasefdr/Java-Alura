499c61876d881fd434661781bad7a4fadf3d4bd1
Lucas <lucas.efdrosa@gmail.com>
2022-12-03 16:08:54 -0300

    Wrapper - Integer

--- src/com/github/lucasefdr/B06JavaUtil/util/WrapperEx.java
+++ src/com/github/lucasefdr/B06JavaUtil/util/WrapperEx.java
@@ -11,5 +11,22 @@ public class WrapperEx {
 
         // Autoboxing -> o Java transforma o tipo primitivo int para Integer
         idadesList.add(idade);
+
+        // Convertendo de tipo primitivo para wrapper - Autoboxing
+        Integer idadeWrapper = Integer.valueOf(idade);
+        // Convertendo de wrapper para tipo primitivo - Unboxing
+        int idadePrimitivo = idadeWrapper.intValue();
+
+        String anoString = "1997";
+        Integer anoInteger = Integer.valueOf(anoString);
+        int anoInt = Integer.parseInt(anoString);
+
+        System.out.println(anoInteger);
+        System.out.println(anoInt);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.BYTES);
+        System.out.println(Integer.SIZE);
+        System.out.println(Integer.TYPE);
     }
 }
