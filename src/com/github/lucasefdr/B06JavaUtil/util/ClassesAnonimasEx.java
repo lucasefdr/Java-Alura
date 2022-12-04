b0c7f9d08cae75188521649cfd750d9a3022cec9
Lucas <lucas.efdrosa@gmail.com>
2022-12-03 21:42:02 -0300

    lambda and method reference

--- src/com/github/lucasefdr/B06JavaUtil/util/ClassesAnonimasEx.java
+++ src/com/github/lucasefdr/B06JavaUtil/util/ClassesAnonimasEx.java
@@ -6,31 +6,30 @@ import com.github.lucasefdr.B06JavaUtil.model.ContaCorrente;
 import com.github.lucasefdr.B06JavaUtil.model.ContaPoupanca;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-public class ClassesAnonimas {
+public class ClassesAnonimasEx {
     public static void main(String[] args) {
-        Conta cc1 = new ContaCorrente(22, 33);
+        Conta cc1 = new ContaCorrente(90, 33);
         Cliente clienteCC1 = new Cliente();
         clienteCC1.setNome("Nico");
         cc1.setTitular(clienteCC1);
         cc1.deposita(333.0);
 
-        Conta cc2 = new ContaPoupanca(22, 44);
+        Conta cc2 = new ContaPoupanca(33, 44);
         Cliente clienteCC2 = new Cliente();
         clienteCC2.setNome("Guilherme");
         cc2.setTitular(clienteCC2);
         cc2.deposita(444.0);
 
-        Conta cc3 = new ContaCorrente(22, 11);
+        Conta cc3 = new ContaCorrente(43, 11);
         Cliente clienteCC3 = new Cliente();
         clienteCC3.setNome("Paulo");
         cc3.setTitular(clienteCC3);
         cc3.deposita(111.0);
 
-        Conta cc4 = new ContaPoupanca(22, 22);
+        Conta cc4 = new ContaPoupanca(12, 22);
         Cliente clienteCC4 = new Cliente();
         clienteCC4.setNome("Ana");
         cc4.setTitular(clienteCC4);
@@ -43,12 +42,29 @@ public class ClassesAnonimas {
         listaDeContas.add(cc3);
         listaDeContas.add(cc4);
 
-        // Implementando uma classe anônima
-        listaDeContas.sort(new Comparator<Conta>() {
-            @Override
-            public int compare(Conta c1, Conta c2) {
-                return Integer.compare(c1.getNumero(), c2.getNumero());
-            }
+        // method reference
+        listaDeContas.forEach(System.out::println);
+
+        // Implementando uma função lambda para a classe Comparator usando o sort de String
+        listaDeContas.sort((c1, c2) -> {
+            String nomeTitularC1 = c1.getTitular().getNome();
+            String nomeTitularC2 = c2.getTitular().getNome();
+            return nomeTitularC1.compareTo(nomeTitularC2);
         });
+
+        System.out.println();
+
+        // method reference
+        listaDeContas.forEach(System.out::println);
+
+        // Implementando uma função lambda para a classe Comparator usando o sort de Integer
+        listaDeContas.sort((Conta c1, Conta c2) -> Integer.compare(c1.getNumero(), c2.getNumero()));
+
+        System.out.println();
+
+        // method reference
+        listaDeContas.forEach(System.out::println);
+
+        listaDeContas.sort(Comparator.comparingInt(Conta::getNumero));
     }
 }
