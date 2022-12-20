0fbb4de2302751582d971cbb267f91a9c8d0dec8
Lucas <lucas.efdrosa@gmail.com>
2022-12-19 21:36:19 -0300

    Java 8 - method reference

--- src/com/github/lucasefdr/B09Java8/A01OrdenaString.java
+++ src/com/github/lucasefdr/B09Java8/A01OrdenaString.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class A01OrdenaString {
     public static void main(String[] args) {
@@ -34,6 +35,12 @@ public class A01OrdenaString {
 
         System.out.println();
 
+        // Usando method reference com o default method comparing()
+        System.out.print("method reference");
+        palavras.sort(Comparator.comparing(String::length));
+
+        System.out.println();
+
         // Modo antigo de percorrer uma lista com foreach
         for (String palavra : palavras) {
             System.out.println(palavra);
@@ -58,6 +65,17 @@ public class A01OrdenaString {
 
         // Usando reference method
         palavras.forEach(System.out::println);
+
+        System.out.println();
+
+        // Nova interface Function
+        // Dado uma String, me retorne um integer
+        Function<String, Integer> function = string -> string.length();
+        System.out.println(function.apply("Lucas"));
+
+        // Usando method reference
+        Function<String, Integer> functioMethodRef = String::length;
+        System.out.println(functioMethodRef.apply("Eduardo"));
     }
 }
 
