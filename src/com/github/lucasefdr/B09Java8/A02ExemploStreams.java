c13e1713ec138c88b7c31deeb1f8956e5e01f6c1
Lucas <lucas.efdrosa@gmail.com>
2022-12-20 07:00:14 -0300

    Java 8 - Collectors

--- src/com/github/lucasefdr/B09Java8/A02ExemploStreams.java
+++ src/com/github/lucasefdr/B09Java8/A02ExemploStreams.java
@@ -1,8 +1,8 @@
 package com.github.lucasefdr.B09Java8;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+import java.util.Map.*;
+import java.util.stream.Collectors;
 
 class Curso {
     private String nome;
@@ -46,9 +46,17 @@ public class A02ExemploStreams {
         System.out.println("*** STREAMS ***");
         System.out.println("Imprimindo todos os cursos com mais de 80 alunos");
         cursos.stream()
-                .filter(c -> c.getAlunos() >= 80)
                 .map(Curso::getAlunos)
+                .filter(alunos -> alunos >= 80)
                 .forEach(System.out::println);
+
+        System.out.println("Média de alunos");
+        OptionalDouble average = cursos.stream()
+                .filter(c -> c.getAlunos() >= 80)
+                .mapToInt(Curso::getAlunos)
+                .average();
+
+        System.out.println(average);
 
         System.out.println("Soma de alunos dos cursos com mais de 80 matriculados");
         int sum = cursos.stream()
@@ -58,6 +66,38 @@ public class A02ExemploStreams {
 
         System.out.println(sum);
 
+        System.out.println();
 
+        System.out.println("Optional");
+        Optional<Curso> optionalCurso = cursos.stream()
+                .filter(c -> c.getAlunos() >= 100)
+                .findAny();
+
+        // Se existir, imprima-o
+        optionalCurso.ifPresent(c -> System.out.println(c.getNome()));
+
+        System.out.println();
+
+        System.out.println("Collectors");
+        // Nova lista de curso com aqueles que tem mais de 90 alunos
+        List<Curso> listDeCursosComMaisDe90Alunos = cursos.stream()
+                .filter(c -> c.getAlunos() >= 90)
+                .collect(Collectors.toList());
+
+        List<Curso> listDeCursosComMaisDe90AlunosJava16 = cursos.stream()
+                .filter(c -> c.getAlunos() >= 90)
+                .toList();
+
+
+        // Novo map de curso com aqueles que tem mais de 90 alunos
+        Map<String, Integer> mapDeCursosComMaisDe90Alunos = cursos.stream()
+                .filter(c -> c.getAlunos() >= 90)
+                .collect(Collectors.toMap(Curso::getNome, Curso::getAlunos));
+
+        Set<Entry<String, Integer>> associacoes = mapDeCursosComMaisDe90Alunos.entrySet();
+        associacoes.forEach(c -> {
+            System.out.println(c.getKey());
+            System.out.println(c.getValue());
+        });
     }
 }
